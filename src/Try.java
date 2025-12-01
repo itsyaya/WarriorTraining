b1150a23f038be52c5785363d9b5b99a8c16f651
Yahya <home@Yahyas-Macbook.local>
2025-12-01 22:52:28 +0100

    _

--- src/Try.java
+++ src/Try.java
@@ -1,4 +1,4 @@
-public class App {
+public class Try {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, Let's create a small Coposition Project");
     }
